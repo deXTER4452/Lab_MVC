b58b5d67e6ce3b316da43223d9f8636fd3a16243
Abu Hasnat <hasnat.tushar@gmail.com>
2022-07-11 05:50:32 -0500

    Lab 3 final

---
+++ Lab_MVC/Controllers/HotelController.cs
@@ -0,0 +1,38 @@
+﻿using Lab_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_MVC.Controllers
+{
+    public class HotelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AllRooms()
+        {
+            return View();
+        }
+
+        public IActionResult AllRooms()
+        {
+             return View(Hotel.Rooms);  
+        }
+
+        public IActionResult TotalCapacity()
+        {
+            ViewBag.totalCapacity = Hotel.TotalCapacityRemaining();
+            return View();
+        }
+
+        public IActionResult Room(int? guests)
+        {
+            Room room = Hotel.Rooms.First(r => r.Capacity >= guests);
+            return View(room);
+        }
+
+    }
+}
+
+//couldn't figure out the errors
